0ff54799c4d357287bd5e9e12a99e8ae4aed65c3
jeanFrancis <nfrancis2013@gmail.com>
2022-01-18 16:33:11 +0100

    V2

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ let age = 15;
 /*
     condition ? valeur respecté : cas contraire
 */
-    age < 0  ? console.log("Age invalid") : console.log("Age valide");
+    age < 0  ? console.log("Age invalids") : console.log("Age valide");
     (age < 18 && age > 0 ) ? console.log("Vous êtes Mineur") : console.log("Majeur");
 
     if( age < 0 ){
@@ -21,3 +21,31 @@ let age = 15;
         console.log("Vous êtes Majeur");
     }
             
+
+    // Switch case
+    let jour = 2;
+        switch (jour){
+            case 1 :
+                    console.log("Lundi");
+                     break;
+            case 2 :
+                    console.log("Mardi");
+                     break;
+            case 3 :
+                    console.log("Mercredi");
+                    // break;
+            case 4 :
+                    console.log("Jeudi");
+                    // break;
+            case 5 :
+                    console.log("Vendredi");
+                    // break;
+            case 6 :
+                    console.log("Samedi");
+                    // break;
+            case 7 :
+                    console.log("Dimanche");
+                     break;
+            default :
+                console.log("Mauvaise Valeur");
+        }
