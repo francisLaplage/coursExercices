ff1da8999675f6bb726d6fa20b83584a3e8e1d53
jeanFrancis <nfrancis2013@gmail.com>
2022-01-24 16:28:15 +0100

    Avec la POO V1

--- js/poo.js
+++ js/poo.js
@@ -40,6 +40,8 @@ class Produit{
         this.designation = nouveau_nom;
     }
     setPrix(nouveau_prix){
+        if(nouveau_prix < 0)
+            throw new Error('Le prix d\'un produit ne peut être négatif');
         this.prix = nouveau_prix;
     }
     //méthode equals : comparer deux objets
@@ -114,3 +116,134 @@ for(let i = 0 ; i< t.length;i++){
 for(let val in t){
     console.log(val);
 }
+
+/*****************************************
+ * 
+ *              Héritage         
+ * 
+ *****************************************/
+
+class Ordinateur extends Produit{
+    constructor(numero,nom,designation,prix,stock,date_fabri,tab_fsseur,taille){
+        super(numero,nom,designation,prix,stock,date_fabri,tab_fsseur);
+        this.taille = taille;
+    }
+    getNom(){
+        return "Je suis la fonction dans la classe enfant (Ordinateur)";
+    }
+}
+
+let ordi = new Ordinateur(15,"HP","ordinateur Portable",1000,45,new Date(),["Simpson"],17);
+console.log(ordi);
+
+//appel de getNom()
+    console.log(ordi.getNom());
+
+
+/*******************************************************
+ * 
+ *                  Objet Littéral : JSON
+ * 
+ *******************************************************/
+
+    let p = {
+                numero : 12,
+                nom : "Java 8",
+                designation : "Livre avec excercices corrigés",
+                prix : 25,
+                stock :145,
+                date_fabri : {
+                    jour : 24,
+                    mois : 1,
+                    annee : 2022
+                },
+                fournisseurs :["Ali","Jean"],
+
+                afficher : function () {
+                    console.log("Je suis un objet Littéral");
+                },
+                getNom : function(){
+                    return this.nom;
+                }
+            }   
+//
+p.afficher();
+// Afficher la date de fabrication de l'objet p
+console.log(p.date_fabri.annee);
+
+//
+console.log(p.fournisseurs[1]);
+
+/****************************************************
+ *                                                  *
+ *      Création de classe par fonction             *
+ *                                                  *
+ ****************************************************/
+
+function Produit2(numero,nom){
+    // this.numeroProd = numero;
+    var numeroProd = numero; //var rend l'attribut privé
+    this.nomProd = nom;     // this rend l'attribut public
+
+    this.getNumero = function() {
+        return numeroProd;
+    }
+    this.getNom = function () {
+        return this.nomProd;
+    }
+    this.setNom = function (nouveau_nom) {
+        this.nomProd = nouveau_nom;
+    }
+}
+let p2 = new Produit2(12,"JS et le DOM");
+console.log(p2);
+//accès aux propriétés 
+console.log(p2.numeroProd); // undefined parce que l'attribut est privé
+console.log(p2.getNumero());
+// Test de setteur de l'approche par fonction
+p2.setNom("Le DOM HTML");
+console.log(p2.getNom());
+
+// Ajout de métodes et d'attribut après la création d'une classe
+
+Produit2.prototype.prixProd = 0; // Ajout de l'attribut prixProd dans la classe
+                                //  Produit2
+Produit2.prototype.getPrixProd = function() { // ajout de la méthode getPrixProd
+    return this.prixProd;                     // Qui retourne le prix du produit  
+}
+
+// appel de la nouvelle méthode sur l'objet p2
+console.log(p2.getPrixProd());
+
+//Héritage avec l'approche par fonction
+var ordi2 = Object.create(Produit2); 
+
+// let ordi2 = new Ordinateur2();
+
+console.log(ordi2);
+
+
+/**************************************************
+ * 
+ *          Gestion des Exceptions
+ *  
+ ************************************************/
+// console.log(k);
+
+function divD(a,b){
+
+    if(b==0){
+        throw new Error("Division par zéro impossible");
+    }
+
+    return a/b;
+}
+let x =15, y=5;
+try{
+    console.log(divD(x,y));
+}catch(err){
+    console.log(err.message)
+}finally{
+    console.log("Quoi d'il arrive");
+}
+
